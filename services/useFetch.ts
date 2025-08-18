969356ae1dbb157f2f067495e3a9fa77e8f9f147
ilhamyusuf27 <ilhamyusuf276@gmail.com>
2025-08-18 21:47:34 +0700

    feat: implement custom useFetch hook for data fetching

---
+++ services/useFetch.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+const useFetch = <T>(fetchFn: () => Promise<T>, autoFetch = true) => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const result = await fetchFn();
+      setData(result);
+    } catch (err) {
+      setError(err instanceof Error ? err : new Error("An error occured"));
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const reset = () => {
+    setData(null);
+    setLoading(false);
+    setError(null);
+  };
+
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData();
+    }
+  }, []);
+
+  return { data, loading, error, refetch: fetchData, reset };
+};
+
+export default useFetch;
